0c6d7bb1d2d3ab6b69c468789aba8ba7be0d3cba
Betina Perez Neder <betinap@protonmail.com>
2024-06-15 15:55:08 -0300

    Handle the User Name as a variable

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,11 @@
 let EMAIL_ADDRESS;
 let PASSWORD;
+let USER_NAME;
 describe("Regular login", () => {
   beforeEach(() => {
     EMAIL_ADDRESS = Cypress.env("email");
     PASSWORD = Cypress.env("password");
+    USER_NAME = Cypress.env("userName");
 
     cy.session(EMAIL_ADDRESS, () => {
       cy.log(`**--- Log in with regular user's credentials ---**`);
@@ -13,9 +15,9 @@ describe("Regular login", () => {
     });
   });
 
-  it(`Checks a regular User can access the app`, () => {
+  it('Checks a regular User can access the app', () => {
     cy.visit(`${Cypress.config().baseUrl}/en/products`);
     cy.url().should("include", "/products");
-    cy.get(".mat-list-text").should("have.text", "Sample User");
+    cy.get(".mat-list-text").should("have.text", USER_NAME);
   });
 });
