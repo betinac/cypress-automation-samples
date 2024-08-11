07d3513d623f4f8b9ff2e938b1544671784253af
Betina Perez Neder <betinap@protonmail.com>
2024-08-10 23:54:39 -0300

    Optimize type command with delay:0

--- cypress/support/commands/login/login.js
+++ cypress/support/commands/login/login.js
@@ -1,8 +1,8 @@
 Cypress.Commands.add("loginUI", (email, password, tenantID) => {
   cy.visit(`${Cypress.config().baseUrl}/en/login`);
-  cy.get("[data-cy='tenant-input']").type(tenantID);
-  cy.get("[data-cy='login-input']").type(email, { log: false });
-  cy.get("[data-cy='password-input']").type(password, { log: false });
+  cy.get("[data-cy='tenant-input']").type(tenantID, { delay: 0 });
+  cy.get("[data-cy='login-input']").type(email, { log: false, delay: 0 });
+  cy.get("[data-cy='password-input']").type(password, { log: false, delay: 0 });
   cy.get("[data-cy='login-button']").click();
 });
 
