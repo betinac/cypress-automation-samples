c716fa2289ddbf75484fd0947e9303e134c9504c
Betina Perez Neder <28940874+betinac@users.noreply.github.com>
2024-08-16 19:06:24 -0300

    Mock login response (#10)
    * Add scripts to run tests from the CLI
    
    * Optimize type command with delay:0
    
    * Add new test to mock login response
    
    * videoUploadOnPasses config option was removed in Cypress v13
    
    * Add new login test but mock the response
    
    ---------
    
    Co-authored-by: Betina Perez Neder <betinap@protonmail.com>

---
+++ cypress/e2e/login/testbench-cloud/alternative-scenarios/mock-failed-login.spec.js
@@ -0,0 +1,31 @@
+describe("Attempt to log in via the UI while mocking the response", () => {
+  const login = {
+    emailAddress: Cypress.env("email"),
+    password: Cypress.env("password"),
+    userName: Cypress.env("userName"),
+    tenantID: Cypress.env("tenantID"),
+  };
+
+  it("Should display an error message even with valid credentials", () => {
+    // Mock the login API response as being unauthorized
+    cy.intercept("POST", "/api/tenants/login/session", {
+      statusCode: 403,
+      body: {
+        failureType: "LoginFailedIncorrectData",
+        message: "Incorrect workspace, login or password.",
+      },
+    }).as("validateUser");
+
+    // Enter valid credentials, the user should not be logged in
+    // as we have mocked the response of the intercepted API
+    cy.loginUI(login.emailAddress, login.password, login.tenantID);
+    cy.waitAndAssertStatusCode("validateUser", 403);
+
+    // Validate that the error message is displayed
+    cy.contains(
+      "Please enter a matching set of workspace, user login and password."
+    ).should("be.visible");
+    // Verify that the URL has not changed
+    cy.url().should("include", "/login");
+  });
+});
